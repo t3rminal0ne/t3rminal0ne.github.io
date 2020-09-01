44efad6798be2761149370a484e68e1da9af9f07
Tiberius Colina <ty_colina22@skynetComputer.local>
2020-09-01 08:14:44 -0400

    added pictures and added navbuttons

--- media/media.js
+++ media/media.js
@@ -2,3 +2,18 @@ const logoImage = document.getElementById('logo')
 logoImage.addEventListener('click', ()=>{
     window.location = "../index.html"
 })
+
+const navbuttonRPC = document.getElementById('nav-div-button-rpc')
+navbuttonRPC.addEventListener('click', ()=>{
+    window.location = "#content-holder-photos-rpc"
+})
+
+const navbuttonDP = document.getElementById('nav-div-button-dp')
+navbuttonDP.addEventListener('click', ()=>{
+    window.location = "#content-holder-photos-dp"
+})
+
+const navbuttonRPCPA = document.getElementById('nav-div-button-rpcpa')
+navbuttonRPCPA.addEventListener('click', ()=>{
+    window.location = "#content-holder-photos-rpcpa"
+})
