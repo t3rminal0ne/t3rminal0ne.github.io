c86891e6dbbb7250f30bff2e86d1acc2f0dca9a9
Tiberius Colina <ty_colina22@skynetComputer.local>
2020-10-03 22:25:43 -0400

    revamped front page again

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ logoImage.addEventListener('click', ()=>{
     window.location = "./index.html"
 })
 
-const seeEventsButton = document.getElementById('see-events-navbutton')
+const seeEventsButton = document.getElementById('info-navbutton-events')
 seeEventsButton.addEventListener('click', ()=>{
     window.location = "./events/events.html"
 })
 
-const seeMediaButton = document.getElementById('see-media-navbutton')
+const seeMediaButton = document.getElementById('info-navbutton-media')
 seeMediaButton.addEventListener('click', ()=>{
     window.location = "./media/media.html"
 })
 
-const seeContactsButton = document.getElementById('learn-more-2-navbutton')
+const seeContactsButton = document.getElementById('info-navbutton-contact')
 seeContactsButton.addEventListener('click', ()=>{
     window.location = "./contacts/contacts.html"
 })
